afa130124010efe2cccc5569ddbb354c3c657105
BV1 <bedoadly12@gmail.com>
2024-12-20 15:57:14 +0300

    Update session 1.py

--- session 1.py
+++ session 1.py
@@ -4,6 +4,28 @@ print("Hello, World!")
 # Variables in Python:
 x = 5
 y = "Hello, World!"
+# Variable Names
+# A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). Rules for Python variables:
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+# A variable name cannot be any of the Python keywords.
+# Legal variable names:
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# Illegal variable names:
+
+# 2myvar = "John"
+# my-var = "John"
+# my var = "John"
+
 
 # Comments in Python:
 
@@ -45,5 +67,22 @@ name = input("What is your name? ")
 #  Displaying user input
 print("Hello, " + name + "! Nice to meet you.")
 
+# task1:
+# Write a code that welcomes the user when the program starts.
+# Use input() to collect the user's name and age.
+# Print a welcome message that includes the user's name.
+# !!optional!! Perform a simple arithmetic operation using numbers entered by the user.
 
+print("welcome to my program")
+name = input("what is your name?")
+age = input("how old are you?")
+print("hello, " + name + "! you are " + age + " years old")
+
+# optional
+# Perform a simple arithmetic operation using numbers entered by the user.
+# Ask the user for two numbers and add them together.
+# Print the result of the addition.
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+print("The sum of " + num1 + " and " + num2 + " is "      + (num1 + num2))
 
