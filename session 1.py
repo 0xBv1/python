bcc4df68e8e48ce5cc4c454ed0ac025417990cf9
BV1 <bedoadly12@gmail.com>
2024-12-20 15:42:40 +0300

    Create session 1.py

---
+++ session 1.py
@@ -0,0 +1,49 @@
+# set's write our first Python file, called helloworld.py, which can be done in any text editor.
+print("Hello, World!")
+
+# Variables in Python:
+x = 5
+y = "Hello, World!"
+
+# Comments in Python:
+
+#This is a comment.
+print("Hello, World!")
+
+print("Hello, World!") #This is a comment
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+
+# Since Python will ignore string literals that are not assigned to a variable, you can add a multiline string (triple quotes) in your code, and place your comment inside it:
+
+# Example
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+# Creating Variables
+# A variable is created the moment you first assign a value to it.
+x = 5
+y = "John"
+print(x)
+print(y)
+
+# Variables do not need to be declared with any particular type, and can even change type after they have been set.
+name = "Python"
+print("I love", name)
+
+# Taking user input
+name = input("What is your name? ")
+
+#  Displaying user input
+print("Hello, " + name + "! Nice to meet you.")
+
+
+
